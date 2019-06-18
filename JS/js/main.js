9295d44daadaed95477f22fa395f3326fbc7dd78
abanoubsamirnaguib <38739589+abanoubsamirnaguib@users.noreply.github.com>
2019-06-18 18:40:47 +0200

    Add files via upload

---
+++ JS/js/main.js
@@ -0,0 +1,41 @@
+//Jquery
+$(document).ready(function (){
+   //fix image size
+		var $winwidth = $(window).width();
+		var $winheight = $(window).height();
+                var $winNav = $('#navbar-example2').height(); 
+		$("#first , .po-bg-dark ").css({
+			width: $winwidth ,
+                        height: $winheight - $winNav 
+                        
+		});
+                console.log("width = " + $winwidth + "height = "+ $winheight );
+		$(window).on("resize", function(){ 
+			$("#first , .po-bg-dark ").css({
+				width: $winwidth,
+                                height: $winheight - $winNav   
+			});                   
+		 }); 
+  
+                 $('.prog1').animate({
+                    width:"50%",
+                    "aria-valuenow":"10"
+             },1000);
+             
+             $(window).scroll(function (){
+//                 navbar2
+                var sec = $(".about-me").offset().top ;              
+                if( $(window).scrollTop() >= sec ) {
+                $("#navbar2").slideDown(1000);}
+            else{$("#navbar2").fadeOut(1);}
+
+//          if($(window).width() < 767)
+//          {
+//              $(".po-first .about-me .button").text("C.V");
+//          }
+        if (window.matchMedia('(max-width: 767px)').matches) { 
+                          $(".po-first .about-me .button").html("C.V");
+          }
+        
+});
+});
